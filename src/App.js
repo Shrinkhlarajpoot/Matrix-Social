5bf31010cdd77ad3625dc10108c4a8998ee96355
Shrinkhla Rajpoot <shrinkhlarajpoot@gmail.com>
2022-05-17 10:00:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { AppRoutes } from "./routes/AppRoutes";
 import "./App.css";
 
-import { useSelector } from "react-redux";
 function App() {
- const {darkTheme}=useSelector((state)=>state.user);
- return (
-    <div className={`App p-0 m-0 ${darkTheme?"dark":""}`} >
-
-
-    <AppRoutes/>
+  const { darkTheme } = useSelector((state) => state.user);
+  return (
+    <div className={`App p-0 m-0 ${darkTheme ? "dark" : ""}`}>
+      <AppRoutes />
     </div>
   );
 }
